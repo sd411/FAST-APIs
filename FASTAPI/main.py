b2977dc8f0fa6d565224ab81d2b13792c96361a8
Shubh Dhyani <42299658+sd411@users.noreply.github.com>
2021-08-11 20:09:54 +0530

    Add files via upload

---
+++ FASTAPI/main.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from fastapi import FastAPI
+import re
+import smtplib
+import dns.resolver
+import json
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/verify/{email}")
+def check_dom(email: str):
+    splitAddress = email.split('@')
+    domain = str(splitAddress[1])
+    print('Domain:', domain)
+    
+    # MX record lookup
+    records = dns.resolver.resolve(domain, 'MX')
+    mxRecord = records[0].exchange
+    mxRecord = str(mxRecord)
+    if "yahoo" in mxRecord:
+        prov = "Yahoo"
+    elif "google" in mxRecord or "gmail" in mxRecord:
+        prov = "Google"
+    elif "microsoft" in mxRecord or "outlook" in mxRecord:
+        prov = "Microsoft"
+    else:
+        prov = "Others"
+
+    return {"Email Mxrecord" : prov}
